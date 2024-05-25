5f44f231ba164008525d8b7b5138e418136fcf5c
Notizen__ <66460003+notizen-00@users.noreply.github.com>
2024-05-25 17:24:01 +0700

    test

--- lib/core/config/config.dart
+++ lib/core/config/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String baseUrl = 'http://10.128.3.114/api/';
+  static const String baseUrl = 'http://10.10.10.144/api/';
   static const String loginUrl = '$baseUrl/auth/login';
   static const String currentUserUrl  = '$baseUrl/user';
 }
